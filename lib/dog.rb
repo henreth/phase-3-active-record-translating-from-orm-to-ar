c3f44164f1082d0409b10286fd9698f4d1426ace
Henry Miller <mail.henrym+git@gmail.com>
2022-02-25 15:18:51 -0500

    commit tree f93a8b59b6d14fd0af4633527a8c67b2eab15c1e

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,22 @@
-class Dog
+require "active_record"
+
+# Setup a database connection
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "db/students.sqlite"
+)
+
+# Create a Students table
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+  )
+SQL
+ActiveRecord::Base.connection.execute(sql)
+
+# Log SQL output to the terminal
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+class Dog < ActiveRecord::Base
 end
